a296b226535e454f71202a387c8f02e325e47e3e
LeoZinger <leo.zinger@gmail.com>
2022-07-08 23:28:07 -0700

    refactored openLock.py into BFS algo as needcode suggested. (2st draft BFS works).

--- venv/openLock.py
+++ venv/openLock.py
@@ -1,25 +1,31 @@
 from typing import List
+from collections import deque
 
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         lock = "0000"
+        if lock in deadends:
+            return -1
         visited = set(deadends)
-        def dfs(lock: List[str], minTurns: int) -> int:  # returns mininum turns
-            if not lock or len(lock) == 0:
-                return 0
-            if lock not in visited:
-                visited.add(lock)
-                if lock == target:
-                    return minTurns
-            minNext = 0
+        q = deque()
+        q.append(("0000", 0))
+
+        def children(lock: str) -> List[str]:
+            res = []
             for i in range(4):
-                minNext = min(minNext, dfs(lock[:i] + str((int(lock[i]) + 1) % 10) + lock[i + 1:], minTurns + 1))    #this won't work, total recusion depth (>1k)exceeding Python's max recursion depth (1000)
-                minNext = min(minNext, dfs(lock[:i] + str((int(lock[i]) - 1 + 10) % 10) + lock[i + 1:], minTurns + 1))
+                res.append(lock[:i] + str((int(lock[i]) + 1) % 10) + lock[i + 1:])
+                res.append(lock[:i] + str((int(lock[i]) - 1 + 10) % 10) + lock[i + 1:])
+            return res
 
-            return minNext + 1
-
-        return dfs(lock, 0)
-
+        while q:
+            lock, numTurns = q.popleft()
+            if lock == target:
+                return numTurns
+            for child in children(lock):
+                if child not in visited:
+                    visited.add(child)
+                    q.append((child, numTurns + 1))
+        return -1
 
 solution = Solution()
 deadends = ["0201","0101","0102","1212","2002"]
