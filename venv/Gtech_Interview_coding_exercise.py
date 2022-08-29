63f4db1087db1052eff2bcebb1fcfbb064b66ac6
LeoZinger <leo.zinger@gmail.com>
2022-08-28 22:56:47 -0700

    2st draft

--- venv/Gtech_Interview_coding_exercise.py
+++ venv/Gtech_Interview_coding_exercise.py
@@ -1,23 +1,26 @@
-# How would you get the company with the highest SAT score average, given a CSV file with an individual's name, his/her company and his/her SAT score?
+# How would you get the 'company' with the highest 'sat' score average, given a CSV file with an individual's 'name', his/her 'company' and his/her 'sat' score?
 # Data from CSV file already in iterable object of my choice
 
 def highest_avg_sat_score():
-    
-    sat_scores_list = [{name:"john", company:"google", sat:100)}, {name:"bot", company:"amazon", sat:95}, {name:"todd", company:"microsoft", sat:96}]
-    
+    sat_scores_list = [{'name':'john', 'company':'google', 'sat':100}, {'name':'bot', 'company':'amazon', 'sat':95},
+    {'name':'todd', 'company':'microsoft', 'sat':96}]
+    print("sat_scores_list = ", sat_scores_list[0])
+
     res_map = {}
-    
     res_map_count = {}
-    
-    
+
     for sat_map in sat_scores_list:
+        company_name = sat_map['company']
+        print("company name = ", company_name)
 
-       company = sat_map[company]
+        res_map_count[company_name] = res_map_count.get(company_name, 0) + 1
 
-       res_map[company] = get(res_map[company], 0) + sat_map[sat]
-
-       res_map_count[company] = get(res_map_count[company], 0) + 1
+        res_map[company_name] = (res_map.get(company_name, 0) + sat_map['sat']) / res_map_count[company_name]
+        print("res_map[", company_name, "] = ", res_map[company_name])
+        print("res_map_count[", company_name, "] = ", res_map_count[company_name])
 
     res_list = sorted(res_map.items(), key=lambda x:x[1], reverse=True)
 
-    return res_list[0][1] / res_map_count[res_list[0][0]]
+    return res_list[0][1]
+
+print("highest_avg_sat_score = ", highest_avg_sat_score())
